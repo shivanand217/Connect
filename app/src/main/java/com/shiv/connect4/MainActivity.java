475b9f29d362fa1537b33e4a9155a701218a6027
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-06-07 21:32:17 +0530

    added gameStates

--- app/src/main/java/com/shiv/connect4/MainActivity.java
+++ app/src/main/java/com/shiv/connect4/MainActivity.java
@@ -1,42 +1,62 @@
 package com.shiv.connect4;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public int activePlayer = 0;
+    // initial state nothing in the blocks
+    int[] gameState = {2,2,2,2,2,2,2,2,2};
+
+    int[][] winningPositions = {{0,1,2},
+                                {3,4,5},
+                                {6,7,8},
+                                {0,3,6},
+                                {1,4,7},
+                                {2,5,8},
+                                {0,4,8},
+                                {2,4,6}};
+
+    String[] winning = {"012","345","678","036","147","258","048","246"};
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
 
-        counter.setTranslationY(1000f);
-        counter.setImageResource(R.drawable.yellow);
+        String tag = counter.getTag().toString();
+        int Tag = Integer.parseInt(tag);
+        System.out.println(Tag);
 
-        counter.animate()
-                .translationYBy(-1000f)
-                .rotation(1000f)
-                .setDuration(500);
+        Toast.makeText(this, "This tag is "+tag+" !!!", Toast.LENGTH_LONG).show();
+
+        if(gameState[Tag] == 2) {
+            gameState[Tag] = activePlayer;
+
+            counter.setTranslationY(1000f);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1 - activePlayer;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 1 - activePlayer;
+            }
+
+            counter.animate()
+                    .translationYBy(-1000f)
+                    .rotation(1000f)
+                    .setDuration(500);
+        }
     }
-
-    public void dropIn1(View view) {
-        ImageView counter = (ImageView) view;
-
-        counter.setTranslationX(-1000f);
-        counter.setImageResource(R.drawable.red);
-
-        counter.animate()
-                .translationYBy(1000f)
-                .rotation(1000f)
-                .setDuration(1000);
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
 }
